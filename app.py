a8b1569b16fb276a4f1b9c71b47e2ade8cf7bf2f
kobashihiroaki <h.kobashi@g-waters.com>
2022-07-28 15:45:35 +0900

    バカラ修正中

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def setup(player_card, banker_card):
         banker_card.append(random.randint(1, 13))
 
 def betting(pocket):
-    return int(input(f'賭け金を入力してください(1-{pocket}): '))
+    return input(f'賭け金を入力してください(1-{pocket}): ')
 
 def choose():
     template = '{}: {}\n   {}'
@@ -24,7 +24,7 @@ def choose():
         print(template.format(str(i), name, rate))
 
     print('-' * 20)
-    return int(input(f'0-{len(rates)-1}: '))
+    return input(f'0-{len(rates)-1}: ')
 
 def player_card_append(player_card):
     player_card.append(random.randint(1, 13))
@@ -33,129 +33,160 @@ def banker_card_append(banker_card):
     banker_card.append(random.randint(1, 13))
 
 def sum_player_card(player_card):
-    return player_card[0] + player_card[1]
+    player = player_card
+    if player[0] > 9:
+        player[0] = 0
+    if player[1] > 9:
+        player[1] = 0
+    return player[0] + player[1]
 
 def sum_banker_card(banker_card):
-    return banker_card[0] + banker_card[1]
+    banker = banker_card
+    if banker[0] > 9:
+        banker[0] = 0
+    if banker[1] > 9:
+        banker[1] = 0
+    return banker[0] + banker[1]
 
-def third_player(player_card):
-    sum_player = sum_player_card(player_card)
-    if (sum_player) < 6:
+def third_player(player_card, player_score):
+    # 条件に合致すれば3枚目を追加する
+    if (player_score) < 6:
         player_card_append(player_card)
     return player_card
 
-def third_banker(player_card, banker_card):
-    sum_player = sum_player_card(player_card)
-    sum_banker = sum_banker_card(banker_card)
-    if sum_banker < 3:
+def third_banker(player_card, banker_card, player_score, banker_score):
+    # 条件に合致すれば3枚目を追加する
+    if banker_score < 3:
         banker_card_append(banker_card)
-    elif sum_banker == 3:
-        if (sum_player == 6) or (sum_player == 7):
+    elif banker_score == 3:
+        if (player_score == 6) or (player_score == 7):
             banker_card_append(banker_card)
         elif len(player_card) > 2:
             if player_card[2] != 8:
                 banker_card_append(banker_card)
-    elif sum_banker_card == 4:
-        if (sum_player == 6) or (sum_player == 7):
+    elif banker_score == 4:
+        if (player_score == 6) or (player_score == 7):
             banker_card_append(banker_card)
         elif len(player_card) > 2:
             if (player_card[2] != 0) or (player_card[2] != 1) or (player_card[2] != 8) or (player_card[2] != 9):
                 banker_card_append(banker_card)
-    elif sum_banker_card == 5:
-        if (sum_player == 6) or (sum_player == 7):
+    elif banker_score == 5:
+        if (player_score == 6) or (player_score == 7):
             banker_card_append(banker_card)
         elif len(player_card) > 2:
             if (player_card[2] == 4) or (player_card[2] == 5) or (player_card[2] == 6) or (player_card[2] == 7):
                 banker_card_append(banker_card)
-    elif sum_banker_card == 6:
-        if (sum_player == 6) or (sum_player == 7):
+    elif banker_score == 6:
+        if (player_score == 6) or (player_score == 7):
             banker_card_append(banker_card)
         elif len(player_card) > 2:
             if (player_card[2] == 6) or (player_card[2] == 7):
                 banker_card_append(banker_card)
     return banker_card
 
-def calc_player_score(player_card, player_score):
+def calc_player_score(player_card):
     player = player_card
-    if player[0] > 10:
+    # 数字が10以上であれば0にする
+    if player[0] > 9:
         player[0] = 0
-    if player[1] > 10:
+    if player[1] > 9:
         player[1] = 0
+    # カードの枚数が3枚の場合
     if len(player_card) > 2:
         if player[2] > 10:
             player[2] = 0
         player_score = (player[0] + player[1]  + player[2]) % 10
+    # カードの枚数が3枚ではない場合
     else:
         player_score = (player[0] + player[1]) % 10
+    
     return player_score
 
-def calc_banker_score(banker_card, banker_score):
-    banker = banker_score
-    if banker[0] > 10:
+def calc_banker_score(banker_card):
+    # 数字が10以上であれば0にする
+    banker = banker_card
+    if banker[0] > 9:
         banker[0] = 0
-    if banker[1] > 10:
+    if banker[1] > 9:
         banker[1] = 0
+    # カードの枚数が3枚の場合
     if len(banker_card) > 2:
-        if banker[2] > 10:
+        if banker[2] > 9:
             banker[2] = 0
-        banker_score = (banker_card[0] + banker_card[1] + banker_card[2]) % 10
+        banker_score = (banker[0] + banker[1] + banker[2]) % 10
+    # カードの枚数が3枚ではない場合
     else:
-        if banker_card[0] > 10:
-            banker_card[0] = 0
-        if banker_card[1] > 10:
-            banker_card[1] = 0
-        banker_score = (banker_card[0] + banker_card[1]) % 10
+        banker_score = (banker[0] + banker[1]) % 10
+    
     return banker_score
 
 def run():
     pocket = 100
     print('バカラやろうよ！')
-    player_card = []
-    banker_card = []
-    bet = ''
-    winner = ''
-    player_score = 0
-    banker_score = 0
+    # 有り金が尽きるまで繰り返す
     while pocket > 0:
+        player_card = []
+        banker_card = []
+        # プレイヤー、バンカー、タイのいずれかを選ぶ
         choice = choose()
+        while not str.isnumeric(choice):
+            print('数字を入れてね')
+            choice = choose()
+        while not(int(choice) == 0) or (int(choice) == 1) or (int(choice) == 2):
+            print('0～2の中から選択してね!')
+            choice = choose()
+        # 数値型に変換
+        choice = int(choice)
+        # 掛け金を入力する
         bet = betting(pocket)
-        while bet <= 0:
-            print('賭け金は1円以上にしてください。')
+
+        if str.isdigit(bet):
+            # 掛け金が0円以下の場合の処理
+            if int(bet) <= 0:
+                print('賭け金は1円以上にしてね!')
+                bet = betting(pocket)
+            # 掛け金が所持金以上の場合の処理
+            elif int(bet) > pocket:
+                print('借金してまで賭けられないよ！')
+                bet = betting(pocket)
+        # 掛け金が数字以外の場合
+        else:
+            print('数字を入れてね!')
             bet = betting(pocket)
-        while bet > pocket:
-            print('借金してまで賭けられないよ！お客さん。')
-            bet = betting(pocket)
+        # 入力した掛け金を数値型に変換
+        bet = int(bet)
+        # プレイヤーのカードを2枚、バンカーのカードを2枚セットする
         setup(player_card, banker_card)
+        # プレイヤーとバンカーのカードを表示する
         print('プレイヤー:' + str(player_card))
         print('バンカー:' + str(banker_card))
-
-        player_score = calc_player_score(player_card, player_score)
-        banker_score = calc_banker_score(banker_card, banker_score)
-
-        player_card = third_player(player_card)
-        banker_card = third_banker(player_card, banker_card)
-
+        # プレイヤー2枚の合計とバンカー2枚の合計を計算する
+        player_score = sum_player_card(player_card)
+        banker_score = sum_banker_card(banker_card)
+        # 3枚目の追加が必要であれば追加する
+        player_card = third_player(player_card, player_score)
+        banker_card = third_banker(player_card, banker_card, player_score, banker_score)
+        # 3枚目が追加された場合、プレイヤーとバンカーのカードを表示する
         if len(player_card) > 2:
-            print('プレイヤーに3枚目のカードが追加されました。:' + str(player_card))
-            if player_card[2] > 10:
-                player_card[2] = 0
-            player_score = calc_player_score(player_card, player_score)
+            print('プレイヤーに3枚目のカードが追加されました。')
+            print('プレイヤー:' + str(player_card))
         if len(banker_card) > 2:
-            print('バンカーに3枚目のカードが追加されました。:' + str(banker_card))
-            if banker_card[2] >10:
-                banker_card[2] = 0
-            banker_score = calc_banker_score(banker_card, banker_score)
-                    
-        print('プレイヤー:' + str(player_score))
-        print('バンカー:' + str(banker_score))
-
-        if banker_score < player_score:        
+            print('バンカーに3枚目のカードが追加されました。')
+            print('バンカー:' + str(banker_card)) 
+        # プレイヤーの合計とバンカーの合計を計算する
+        player_score = calc_player_score(player_card)
+        banker_score = calc_banker_score(banker_card)
+        # プレイヤーとバンカーの合計値を出力する
+        print('プレイヤーの合計:' + str(player_score))
+        print('バンカーの合計:' + str(banker_score))
+        # プレイヤーとバンカーのどちらが勝ちか判定し、所持金の計算をする
+        if player_score > banker_score:        
             print(rates[0][2])
             if (choice == 0):
                 pocket += rates[0][1] * bet
             else:
                 pocket -= bet
-        elif banker_score > player_score:
+        elif player_score < banker_score:
             print(rates[1][2])
             if (choice == 1):
                 pocket += rates[1][1] * bet
